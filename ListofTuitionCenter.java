1b77d45acc9cfcfaf0dc9c485f43029fb8466aba
ifayana <118966979+ifayana@users.noreply.github.com>
2023-03-06 01:58:46 +0800

    Create ListofTuitionCenter.java

---
+++ ListofTuitionCenter.java
@@ -0,0 +1,12 @@
+public class ListofTuitionCenter {
+    int numTC;
+    String Addr;
+    
+    //constructor
+    public ListofTuitionCenter(String Addr, int numTC ){
+        this.Addr=Addr;
+        this.numTC=numTC;
+    }
+    
+    
+}
